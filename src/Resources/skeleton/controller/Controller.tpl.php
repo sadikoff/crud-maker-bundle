692b67b71675e3569f36aa090cd9c006d67aa1ee
dr-matt-smith <dr_matt_smith@me.com>
2018-02-05 10:19:11 +0000

    moved new() method before show() to fix route confusion

--- src/Resources/skeleton/controller/Controller.tpl.php
+++ src/Resources/skeleton/controller/Controller.tpl.php
@@ -29,32 +29,15 @@ class <?= $controller_class_name ?> extends Controller
         return $this->render('<?= $route_name ?>/index.html.twig', ['<?= $entity_var_plural ?>' => $<?= $entity_var_plural ?>]);
     }
 
-    /**
-     * @Route("/{<?= $entity_identifier ?>}", name="show")
-     * @Method("GET")
-     *
-     * @param <?= $entity_class_name ?> $<?= $entity_var_singular ?> The <?= $entity_class_name ?> entity
-     *
-     * @return Response
-     */
-    public function show(<?= $entity_class_name ?> $<?= $entity_var_singular ?>)
-    {
-        $deleteForm = $this->createDeleteForm($<?= $entity_var_singular ?>);
-
-        return $this->render('<?= $route_name ?>/show.html.twig', [
-            '<?= $entity_var_singular ?>' => $<?= $entity_var_singular ?>,
-            'delete_form' => $deleteForm->createView(),
-        ]);
-    }
 
     /**
-     * @Route("/new", name="new")
-     * @Method({"GET", "POST"})
-     *
-     * @param Request $request
-     *
-     * @return Response
-     */
+    * @Route("/new", name="new")
+    * @Method({"GET", "POST"})
+    *
+    * @param Request $request
+    *
+    * @return Response
+    */
     public function new(Request $request)
     {
         $<?= $entity_var_singular ?> = new <?= $entity_class_name ?>();
@@ -72,6 +55,24 @@ class <?= $controller_class_name ?> extends Controller
         return $this->render('<?= $route_name ?>/new.html.twig', [
             '<?= $entity_var_singular ?>' => $<?= $entity_var_singular ?>,
             'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{<?= $entity_identifier ?>}", name="show")
+     * @Method("GET")
+     *
+     * @param <?= $entity_class_name ?> $<?= $entity_var_singular ?> The <?= $entity_class_name ?> entity
+     *
+     * @return Response
+     */
+    public function show(<?= $entity_class_name ?> $<?= $entity_var_singular ?>)
+    {
+        $deleteForm = $this->createDeleteForm($<?= $entity_var_singular ?>);
+
+        return $this->render('<?= $route_name ?>/show.html.twig', [
+            '<?= $entity_var_singular ?>' => $<?= $entity_var_singular ?>,
+            'delete_form' => $deleteForm->createView(),
         ]);
     }
 
